376915141859fab116d0551a6fa319972473c9b1
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 21:39:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-  return $('input').val();
+  return $('input');
 }
 
 retrieveEmployeeInformation();
