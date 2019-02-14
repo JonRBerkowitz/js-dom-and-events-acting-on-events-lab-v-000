6b00c7e5542b40f1a6642e6b9e9e945547684ce0
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 21:51:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
   const button = document.querySelector("input[type='submit']");
-  let field = document.querySelector("input[type='text']");
+  let field = document.querySelector("input[type='text']").val();
   console.log(button);
   console.log(field);
 }
