35198e9a50083530c958f0d63525efad7ad07463
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 22:07:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function addNewElementAsLi() {
   let originalList = document.querySelector('ul');
   return originalList.innerHTML += retrieveEmployeeInformation();
 }
+
+function addNewLiOnClick() {
+  const button = document.querySelector("input[type='submit']");
+  button.addEventListener('click', addNewElementAsLi());
+}
